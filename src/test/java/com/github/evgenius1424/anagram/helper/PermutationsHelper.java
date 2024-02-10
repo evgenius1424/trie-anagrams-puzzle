49505e232964235be42a6d94a3c2534583468016
evgenius1424 <evgenius1424@yandex.ru>
2024-02-10 20:25:03 +0100

    Simpler function to generate permutations

--- src/test/java/com/github/evgenius1424/anagram/helper/PermutationsHelper.java
+++ src/test/java/com/github/evgenius1424/anagram/helper/PermutationsHelper.java
@@ -6,49 +6,21 @@ import java.util.Set;
 public class PermutationsHelper {
 
     public static Set<String> substringsPermutations(String string) {
-        Set<String> set = new HashSet<>();
-        char[] values = string.toCharArray();
-        for (int width = 1; width <= values.length; width++) {
-            int[] stack = new int[width];
-            for (int i = 0; i < stack.length; i++) {
-                stack[i] = stack.length - i - 1;
-            }
-            int position = 0;
-            while (position < width) {
-
-                position = 0;
-                StringBuilder sb = new StringBuilder();
-                while (position < width) {
-                    sb.append(values[stack[position]]);
-                    position++;
-                }
-                set.add(sb.toString());
-                position = 0;
-                while (position < width) {
-                    if (stack[position] < values.length - 1) {
-                        stack[position]++;
-                        if (!containsDuplicate(stack))
-                            break;
-                        else
-                            position = 0;
-                    } else {
-                        stack[position] = 0;
-                        position++;
-                    }
-                }
-            }
-        }
-        return set;
+        Set<String> result = new HashSet<>();
+        generatePermutations("", string, result);
+        return result;
     }
 
-    private static boolean containsDuplicate(int[] stack) {
-        for (int i = 0; i < stack.length; i++) {
-            for (int j = 0; j < stack.length; j++) {
-                if (stack[i] == stack[j] && i != j) {
-                    return true;
-                }
+    private static void generatePermutations(String prefix, String remaining, Set<String> result) {
+        int n = remaining.length();
+        if (n == 0) {
+            result.add(prefix);
+        } else {
+            for (int i = 0; i < n; i++) {
+                generatePermutations(prefix + remaining.charAt(i),
+                        remaining.substring(0, i) + remaining.substring(i + 1, n),
+                        result);
             }
         }
-        return false;
     }
 }
